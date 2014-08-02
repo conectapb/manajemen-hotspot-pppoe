aa50cb78e4a66b3554f6e6c4c1b110edd8edb624
cybercity31@gmail.com <cybercity31@gmail.com@c5adace9-eab8-6789-8881-9285b82bf94d>
2014-08-02 05:39:07 +0000

    menu untuk bagian atas

---
+++ menu-atas.php
@@ -0,0 +1,28 @@
+		<div id="nav">
+			<ul>
+				<li class="upp"><a href="index.php">Beranda</a>
+					<ul>
+						<li>&#8250; <a href="">Monitoring Hotspot</a></li>
+						<li>&#8250; <a href="">Monitoring PPPOE</a></li>
+					</ul>
+				</li>
+				<li class="upp"><a href="#">Sistem Hotspot</a>
+					<ul>
+						<li>&#8250; <a href="">Manajemen Paket</a></li>
+						<li>&#8250; <a href="">Manajemen User</a></li>
+					</ul>
+				</li>
+				<li class="upp"><a href="#">Sistem PPPOE</a>
+					<ul>
+						<li>&#8250; <a href="">Manajemen Paket</a></li>
+						<li>&#8250; <a href="">Manajemen User</a></li>
+					</ul>
+				</li>
+				<li class="upp"><a href="#">Pengaturan</a>
+					<ul>
+						<li>&#8250; <a href="">Manajemen Admin</a></li>
+						<li>&#8250; <a href="">Routers</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
