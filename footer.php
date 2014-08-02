7827bafddaa818821185302410dc430959a50354
cybercity31@gmail.com <cybercity31@gmail.com@c5adace9-eab8-6789-8881-9285b82bf94d>
2014-08-02 05:38:01 +0000

    bagian bawah template

---
+++ footer.php
@@ -0,0 +1,16 @@
+		</div>
+		<div class="clear"></div>
+	</div>
+	
+	<div id="footer">
+		<div class="left">
+			<p>Copyright &copy CyberCity Networks | <b>Hotspot Admin Panel</b></p>
+		</div>
+		<div class="right">
+			<p><a href="http://www.cybercityhost.com" target="_blank">CyberCity Hosting</a> | <a href="http://www.cyberciynetworks.com" target="_blank">CyberCity Website</a></p>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
