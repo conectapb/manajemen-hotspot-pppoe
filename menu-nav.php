89723d96d59965e8998cb932d774abf4e2008a88
cybercity31 <cybercity31@gmail.com>
2014-08-02 05:39:40 +0000

    menu bagian kiri

---
+++ menu-nav.php
@@ -0,0 +1,32 @@
+		<div id="sidebar">
+			<div class="box">
+				<div class="h_title">&#8250; Menu Utama</div>
+				<ul id="home">
+					<li class="b1"><a class="icon view_page" href="index.php">Beranda</a></li>
+					<li class="b2"><a class="icon report" href="">Monitoring Hotspot</a></li>
+					<li class="b2"><a class="icon report" href="">Monitoring PPPOE</a></li>
+
+				</ul>
+			</div>
+			<div class="box">
+				<div class="h_title">&#8250; Sistem Hotspot</div>
+				<ul id="home">
+					<li class="b1"><a class="icon add_page" href="?page=paket">Manajemen Paket</a></li>
+					<li class="b2"><a class="icon users" href="?page=user">Manajemen User</a></li>
+				</ul>
+			</div>
+			<div class="box">
+				<div class="h_title">&#8250; Sistem PPPOE</div>
+				<ul id="home">
+					<li class="b1"><a class="icon config" href="?page=pengguna">Manajemen Paket</a></li>
+					<li class="b2"><a class="icon users" href="?page=route">Manajemen User</a></li>
+				</ul>
+			</div>
+			<div class="box">
+				<div class="h_title">&#8250; Pengaturan</div>
+				<ul id="home">
+					<li class="b1"><a class="icon config" href="?page=pengguna">Manajemen Admin</a></li>
+					<li class="b2"><a class="icon category" href="?page=route">Routers</a></li>
+				</ul>
+			</div>
+		</div>
